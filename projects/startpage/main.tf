68f63455f093692584f163d151e1088922943183
Justin Davis <justindaniel1992@msn.com>
2020-11-22 19:28:55 -0600

    Updating bucket name

--- projects/startpage/main.tf
+++ projects/startpage/main.tf
@@ -4,7 +4,7 @@ module "start_page" {
     source = "../../modules/static-site-s3-cloudfront-noauth"
 
     aws_region = "us-east-1"
-    site_project_name = "testproject"
+    site_project_name = "startpage.justinddavis.com"
     price_class = "PriceClass_100"
     oai_identity_comment = "static-site-s3-cloudfront-noauth"
 }
